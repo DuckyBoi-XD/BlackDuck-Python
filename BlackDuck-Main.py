32bd7c9c25ed3e4a5ddb01d444aeec01cd96b65e
Braeden Sy Tan <braedenjairsytan@icloud.com>
2025-07-29 22:47:36 +1200

    Created mmain python file, Created card variables and other variables

---
+++ BlackDuck-Main.py
@@ -0,0 +1,73 @@
+import random
+
+UserWallet = 1000
+UserHand = 0
+
+H2 = 2
+H3 = 3
+H4 = 4
+H5 = 5
+H6 = 6
+H7 = 7
+H8 = 8
+H9 = 9
+H10 = 10
+HD = 10
+HQ = 10
+HK = 10
+HA = 11 if UserHand >=10 else 1
+
+S2 = 2
+S3 = 3
+S4 = 4
+S5 = 5
+S6 = 6
+S7 = 7
+S8 = 8
+S9 = 9
+S10 = 10
+SD = 10
+SQ = 10
+SK = 10
+SA = 11 if UserHand >=10 else 1
+
+D2 = 2
+D3 = 3
+D4 = 4
+D5 = 5
+D6 = 6
+D7 = 7
+D8 = 8
+D9 = 9
+D10 = 10 
+DD = 10
+DQ = 10
+DK = 10
+DA = 11 if UserHand >=10 else 1
+
+C2 = 2
+C3 = 3
+C4 = 4
+C5 = 5
+C6 = 6
+C7 = 7
+C8 = 8
+C9 = 9
+C10 = 10
+CD = 10
+CQ = 10
+CK = 10
+CA = 11 if UserHand >=10 else 1
+
+CardDeck = (
+    H2, H3, H4, H5, H6, H7, H8, H9, H10, HD, HQ, HK, HA,
+    S2, S3, S4, S5, S6, S7, S8, S9, S10, SD, SQ, SK, SA,
+    D2, D3, D4, D5, D6, D7, D8, D9, D10, DD, DQ, DK, DA,
+    C2, C3, C4, C5, C6, C7, C8, C9, C10, CD, CQ, CK, CA
+)
+
+CurrentCardDeck = CardDeck
+
+print("Welcome to BlackDuck. Just like Blackjack, but with ducks!\nYou start with $1000.")
+
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # BlackJack-Ducky
-Making blackjack
+
+
