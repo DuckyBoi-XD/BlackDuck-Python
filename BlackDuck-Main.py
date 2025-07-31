7e53734574b9d3731b9b391bfaeb0c1fe3b444a9
Braeden Sy Tan <braedenjairsytan@icloud.com>
2025-07-31 22:34:56 +1200

    Updated Main py - Information -- how to play + terminology

--- BlackDuck-Main.py
+++ BlackDuck-Main.py
@@ -2,7 +2,6 @@ import random
 
 UserWallet = 1000
 UserHand = 0
-
 CP1 = 1
 CP2 = 1
 CP3 = 1
@@ -83,24 +82,57 @@ CurrentCardDeck = CardDeck
 def Menu():
     try:
         while True:
-            UserMenuPick = input(
-                "What would you like to do?\n1) Play\n2) Information \n"
-                "3) Settings \n4) Quit\n"
+            UMP = input(
+                "\nWhat would you like to do?\n1) Play\n2) Information \n"
+                "3) Settings \n4) Quit\n\n"
             ).strip().lower()
-            if UserMenuPick == "1" or UserMenuPick == "Play":
+            if UMP in ("1", "play"):
                 print("playing")
-
-            elif UserMenuPick == "2" or UserMenuPick == "Information":
-                    UIP = (
-                        "Information about this game!\n1) How to play (Normal)\n2) How to play (Advanced)\n"
-                        "3) Terminology\n4) Card Values\n5) Tips and Tricks\n 6) Back\n"
+            elif UMP in ("2", "information"):
+                UPExit = False
+                while True:
+                    UIP = input(
+                        "\nInformation about this game!\n1) How to play (Normal)\n2) How to play (Advanced)\n"
+                        "3) Terminology\n4) Card Values\n5) Tips and Tricks\n6) Back\n\n"
                         ).strip().lower()
                     if UIP == "1" or UIP == "How to play (Normal)":
-                        print("") ###### NOT DONE
+                        print("\nThe aim of the game is to get higher than the dealer without going over 21." \
+                        "\nYou are first given 2 cards where you have 2 options (see in terminology), hit: be given another card or stand: don't get anymore cards."
+                        "\nIf the value of the cards that you have are over 21, then your lose."
+                        "\nOnce you 'hit', your total card value will get closer and closer to 21."
+                        "\nIf you your total card value goes over 21, then you lose (bust)"
+                        "\nIf you don't want to recive any more cards you stand"
+                        "\nAfter standing, dealer gets to recive cards to match or achieve a higher total card value."
+                        "\nIf your total card value matchs the dealer's total card value, then no one wins and your bet is returned to you (push)"
+                        "\nIf your total card value is lower than the dealer's total card value, then you win, reciving double your bet amount"
+                        "\nYou win if the dealer bust (like how the payer would)"
+
+                    )
                     elif UIP == "2" or UIP == "How to play (Advanced)":
                         print("") ###### NOT DONE
                     elif UIP == "3" or UIP == "Terminology":
-                        print("") ###### NOT DONE
+                        while True:
+                            UPT = input(
+                                "\nHand: The cards that the payer bets with"
+                                "\nHit: To be given another card\nStand: To revice no more cards"
+                                "\nBust: The value of all your cards is higher than 21 therefore you lose"
+                                "\nPush: If both the dealer and player has the same total value, no one wins and all bets and given back"
+                                "\nBlackjack: The value of all your cards total up to exactly 21, giving you 2.5x your bet ammount (unless push)"
+                                "\n\nDouble Down: After being dealt you can double your bet, but after a hitting won't be able to"
+                                "\nSplit: After being dealt, you can make another hand by using one of the cards given to you (you add the same bet as the first hand)"
+                                "\nSurender: Give up your the hand and recive half of your bet back"
+                                "\n\n1) Back"
+                                "\n2) Menu\n\n"
+                            ).strip().lower()
+                            if UPT in ("1", "back"):
+                                break
+                            elif UPT in ("2", "menu"):
+                                UPExit = True
+                                break
+                            else:
+                                print("\nOption unavaliable, please try again!")
+                        if UPExit:
+                            break
                     elif UIP == "4" or UIP == "Card Values":
                         print("") ###### NOT DONE
                     elif UIP == "5" or UIP == "Tips and Tricks":
@@ -108,15 +140,21 @@ def Menu():
                     elif UIP == "6" or UIP == "Back":
                         break
                     else:
-                        print("\nOption not avaliable\n")
-
+                        print("\nOption unavaliable, please try again!")
+            elif UMP in ("3", "settings"):
+                pass
+            elif UMP in ("4", "quit"):
+                print("\nQuitting Game\n")
+                exit()
+            else:
+                print("\nOption unavaliable, please try again!")
 
     except KeyboardInterrupt:
-        print("Quitting")
+        print("\nQuitting Program\n")
         exit()
 
     except EOFError:
-        print("Quitting")
+        print("\nQuitting Program\n")
         exit()
 
 print("\nWelcome to BlackDuck. Just like Blackjack, but with ducks!\nYou start with $1000.")
@@ -124,4 +162,3 @@ print("\nWelcome to BlackDuck. Just like Blackjack, but with ducks!\nYou start w
 Menu()
 
 
-# How to play (Normal):\nThe aim of the game is to get higher than the dealer without going over 21.\nYou are first given 2 cards where you have 2 options (see in terminology), be given another card or don't get anymore.\nIf the value of the cards that you have are over 21, then your lose.\n")
